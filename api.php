0ee23aead6a530a7aa93aa5944ec29e1cd11ebf5
Berkutta <benjamin.marty@gmail.com>
2018-03-12 14:00:00 +0100

    Add api

---
+++ api.php
@@ -0,0 +1,38 @@
+<?php
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+switch($data["operation"]) {
+
+	case "search":
+
+		$search = $data["search"];
+
+		$mysqli =  mysqli_connect("localhost", "root", "pass", "erp");
+
+		// Check connection
+		if (!$mysqli) {
+		    die("Connection failed: " . mysqli_connect_error());
+		}
+
+		$string = "SELECT DISTINCT * FROM `components` WHERE Description LIKE \"%$search%\" OR Storage LIKE \"%$search%\" OR Category LIKE \"%$search%\" OR Type LIKE \"%$search%\" OR package LIKE \"%$search%\"";
+
+		$res = mysqli_query($mysqli, $string);
+
+		while ($row = mysqli_fetch_assoc($res)) {
+			$myresult["id"] = $row["ID"];
+			$myresult["storage"] = $row["storage"];
+			$myresult["description"] = $row["Description"];
+			$myresult["category"] = $row["Category"];
+			$myresult["stock"] = $row["Stock"];
+
+			$result[] = $myresult;
+		}
+
+		echo json_encode($result);
+
+		break;
+}
+
+?>
+
