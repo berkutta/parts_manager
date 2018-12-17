d0b0ff297ca33e5bc4f87e5971b0e4a687e92478
Benjamin <info@benjaminmarty.ch>
2018-12-17 23:05:22 +0100

    add huge style improvement @xama5 ~13.2.2017
    (cherry picked from commit 7cdd75ba991af880e4423c287de63e3ee7b78672)

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
+session_start();
 
 $username = htmlspecialchars($_POST["username"]);
 $password = md5(htmlspecialchars($_POST["password"]));
 $logout = htmlspecialchars($_POST["logout"]);
-
-session_start();
 
 $config = include('config.php');
 
@@ -31,3 +30,40 @@ if( !empty($username) && !empty($password) ) {
 }
 
 ?>
+
+<style>
+        body {
+                font-family:Calibri;
+        }
+        tr:nth-of-type(even) {
+                background:#efefef;
+        }
+        tr td {
+                padding: 10px;
+                box-sizing: border-box;
+                -webkit-box-sizing:border-box;
+                -moz-box-sizing: border-box;
+        }
+        tr:first-of-type {
+                font-weight:bold;
+        }
+        a, input[type="submit"] {
+                display:inline-block;
+                margin:5px;
+                padding:5px 10px;
+                background:darkgrey;
+                color:#fff;
+                text-decoration:none;
+                border:none;
+        }
+        input, select {
+                padding: 5px 10px;
+                margin:5px;
+                border:none;
+                border-bottom:1px solid lightgrey;
+                outline:none;
+                box-sizing: border-box;
+                -webkit-box-sizing:border-box;
+                -moz-box-sizing: border-box;
+        }
+</style>
--- view.php
+++ view.php
@@ -1,15 +1,53 @@
 <?php
-
 session_start();
 
 $config = include('config.php');
 
 if( $_SESSION["user"] != $config['user'] ) {
-	header("Location: index.php");
+        header("Location: index.php");
 
-	exit;
+        exit;
 }
+?>
 
+<style>
+	body {
+		font-family:Calibri;
+	}
+	tr:nth-of-type(even) {
+		background:#efefef;
+	}
+	tr td {
+		padding: 10px;
+		box-sizing: border-box;
+     		-webkit-box-sizing:border-box;
+     		-moz-box-sizing: border-box;
+	}
+	tr:first-of-type {
+		font-weight:bold;
+	}
+	a, input[type="submit"] {
+		display:inline-block;
+		margin:5px;
+		padding:5px 10px;
+		background:darkgrey;
+		color:#fff;
+		text-decoration:none;
+		border:none;
+	}
+	input, select {
+		padding: 5px 10px;
+		margin:5px;
+		border:none;
+		border-bottom:1px solid lightgrey;
+		outline:none;
+		box-sizing: border-box;
+     		-webkit-box-sizing:border-box;
+     		-moz-box-sizing: border-box;
+	}
+</style>
+
+<?php
 $view = htmlspecialchars($_GET["view"]);
 $param1 = htmlspecialchars($_GET["param1"]);
 
