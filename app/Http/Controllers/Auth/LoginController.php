677d883dca9409eb298392b4d582c7846dcf3d0e
Benjamin Marty <info@benjaminmarty.ch>
2019-01-06 23:10:47 +0100

    add to redirect to the correct path after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/components';
 
     public function username()
     {
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/components';
 
     /**
      * Create a new controller instance.
