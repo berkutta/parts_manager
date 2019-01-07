b3b9b749b8a80ac80cc4acc0bf60404f2cd7c480
Benjamin Marty <info@benjaminmarty.ch>
2019-01-07 19:16:29 +0100

    add sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,35 @@
         </nav>
 
         <main role="main" class="container">
-        @yield('content')
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Sidebar
+                        </div>
+
+                        <div class="card-body">
+                            <ul class="nav nav-pills flex-column" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link active" href="{{ url('/components') }}">
+                                        Components
+                                    </a>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link" href="{{ url('/storages') }}">
+                                        Storages
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-9">
+                    @yield('content')
+                </div>
+            </div>
+
         </main><!-- /.container -->
 
     </body>
