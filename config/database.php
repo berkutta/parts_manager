cf7de99c3ba643942fb7b0c9163ceacde05b46e5
Benjamin Marty <info@benjaminmarty.ch>
2019-01-07 21:54:47 +0100

    add support for mariadb installations

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
+            'driver' => 'mariadb',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
