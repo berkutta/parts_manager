2595ab193bc32994b66d72b5b4d995ec632b4954
Benjamin Marty <info@benjaminmarty.ch>
2019-01-11 16:03:43 +0100

    add support for pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <title>Parts Manager</title>
         <meta name="description" content="Parts Manager">
         <meta name="theme-color" content="#343a40">
+        <link rel="manifest" href="/manifest.json">
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         
