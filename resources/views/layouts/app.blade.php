aab4f1b953cf7fd3d5d350d296f2533deb5891d1
Benjamin Marty <info@benjaminmarty.ch>
2019-01-11 04:36:40 +0100

    add html meta for title and theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
+        <title>Parts Manager</title>
+        <meta name="description" content="Parts Manager">
+        <meta name="theme-color" content="#343a40">
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         
