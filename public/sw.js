6f1c1f1c4662d1ec7231cfa8e5fd7373cf862192
Benjamin Marty <info@benjaminmarty.ch>
2019-01-13 02:39:33 +0100

    add boilerplate serviceworker to be installable like a pwa

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    // Perform install steps
+  })
+  
+  self.addEventListener('fetch', (event) => {
+   // Handle requests
+  })
+  
+  self.addEventListener('activate', (event) => {
+    // Clean up old cache versions
+  })
+  
+  
