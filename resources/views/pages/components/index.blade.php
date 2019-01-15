d20f820bdbc1f9b7c822ccb593e57b877d020ef0
Benjamin Marty <info@benjaminmarty.ch>
2019-01-15 23:39:50 +0100

    fix to not render mobile cards on desktop

--- resources/views/pages/components/index.blade.php
+++ resources/views/pages/components/index.blade.php
@@ -6,87 +6,87 @@
    <div class="col-md-9">
    <a class="btn btn-success create-btn" href="/components/create" role="button">Create</a>
       <div class="table-responsive-md d-none d-md-block">
-      <table class="table table-hover">
-         <thead>
-            <tr>
-               <th scope="col"></th>
-               <th scope="col">Storage</th>
-               <th scope="col">Name</th>
-               <th scope="col">Datasheet</th>
-               <th scope="col">Category</th>
-               <th scope="col">Subcategory</th>
-               <th scope="col">Package</th>
-               <th scope="col">Tags</th>
-               <th scope="col">Stock</th>
-               <th scope="col"></th>
-               <th scope="col"></th>
-               <th scope="col"></th>
-               <th scope="col"></th>
-            </tr>
-         </thead>
+         <table class="table table-hover">
+            <thead>
+               <tr>
+                  <th scope="col"></th>
+                  <th scope="col">Storage</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Datasheet</th>
+                  <th scope="col">Category</th>
+                  <th scope="col">Subcategory</th>
+                  <th scope="col">Package</th>
+                  <th scope="col">Tags</th>
+                  <th scope="col">Stock</th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+               </tr>
+            </thead>
 
-         <tbody>
-            @foreach ($entries as $entry)
-            <tr>
-               @if (file_exists("preview/part_".$entry->id.".jpg"))
-               <td><img src="/preview/part_{{ $entry->id }}.jpg" height="42"></td>
-               @else
-               <td></td>
-               @endif
-               <td>{{ $entry->storage['name'] }}</td>
-               <td>{{ $entry->name }}</td>
-               @empty($entry->extra_attributes['datasheet'])
-               <td></td>
-               @else
-               <td><a class="btn btn-primary" href="{{ $entry->extra_attributes['datasheet'] }}">Link</a></td>
-               @endempty
-               <td>{{ $entry->category }}</td>
-               <td>{{ $entry->extra_attributes['subcategory'] }}</td>
-               <td>{{ $entry->extra_attributes['package'] }}</td>
-               <td>
-               @foreach ($entry->tags as $tag)
-                  <span class="badge badge-primary">{{ $tag->slug }}</span>
+            <tbody>
+               @foreach ($entries as $entry)
+               <tr>
+                  @if (file_exists("preview/part_".$entry->id.".jpg"))
+                  <td><img src="/preview/part_{{ $entry->id }}.jpg" height="42"></td>
+                  @else
+                  <td></td>
+                  @endif
+                  <td>{{ $entry->storage['name'] }}</td>
+                  <td>{{ $entry->name }}</td>
+                  @empty($entry->extra_attributes['datasheet'])
+                  <td></td>
+                  @else
+                  <td><a class="btn btn-primary" href="{{ $entry->extra_attributes['datasheet'] }}">Link</a></td>
+                  @endempty
+                  <td>{{ $entry->category }}</td>
+                  <td>{{ $entry->extra_attributes['subcategory'] }}</td>
+                  <td>{{ $entry->extra_attributes['package'] }}</td>
+                  <td>
+                  @foreach ($entry->tags as $tag)
+                     <span class="badge badge-primary">{{ $tag->slug }}</span>
+                  @endforeach
+                  <td>{{ $entry->stock }}</td>
+                  @if ($entry->stock_flag == 1)
+                  <td class="control-element">
+                     <form action="/components/{{ $entry->id }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="stock" value="+1" />
+                        <input type="hidden" name="_method" value="put" />
+                        <input class="btn btn-primary" type="submit" value="+" />
+                        <br/>
+                     </form>
+                  </td>
+                  <td class="control-element">
+                     <form action="/components/{{ $entry->id }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="stock" value="-1" />
+                        <input type="hidden" name="_method" value="put" />
+                        <input class="btn btn-primary" type="submit" value="-" />
+                        <br/>
+                     </form>
+                  </td>
+                  @else
+                  <td></td>
+                  <td></td>
+                  @endif
+                     <td class="control-element"><a class="btn btn-primary" href="/components/{{ $entry->id }}/edit"><i class="fas fa-edit"></i></a></td>
+                  <td class="control-element">
+                     <form onsubmit="return confirm('Do you really want to delete {{ $entry->name }}?');" action="/components/{{ $entry->id }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="_method" value="delete" />
+                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                        <br/>
+                     </form>
+                  </td>
+               </tr>
                @endforeach
-               <td>{{ $entry->stock }}</td>
-               @if ($entry->stock_flag == 1)
-               <td class="control-element">
-                  <form action="/components/{{ $entry->id }}" method="POST">
-                     @csrf
-                     <input type="hidden" name="stock" value="+1" />
-                     <input type="hidden" name="_method" value="put" />
-                     <input class="btn btn-primary" type="submit" value="+" />
-                     <br/>
-                  </form>
-               </td>
-               <td class="control-element">
-                  <form action="/components/{{ $entry->id }}" method="POST">
-                     @csrf
-                     <input type="hidden" name="stock" value="-1" />
-                     <input type="hidden" name="_method" value="put" />
-                     <input class="btn btn-primary" type="submit" value="-" />
-                     <br/>
-                  </form>
-               </td>
-               @else
-               <td></td>
-               <td></td>
-               @endif
-                  <td class="control-element"><a class="btn btn-primary" href="/components/{{ $entry->id }}/edit"><i class="fas fa-edit"></i></a></td>
-               <td class="control-element">
-                  <form onsubmit="return confirm('Do you really want to delete {{ $entry->name }}?');" action="/components/{{ $entry->id }}" method="POST">
-                     @csrf
-                     <input type="hidden" name="_method" value="delete" />
-                     <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                     <br/>
-                  </form>
-               </td>
-            </tr>
-            @endforeach
-         </tbody> 
-      </table>
+            </tbody> 
+         </table>
       </div>
 
-      <div class="td-md-none">
+      <div class="d-md-none">
          @foreach ($entries as $entry)
          <div class="card card-margin">
             <div class="card-body">
